bef1681e49d57e5f079a1039fd568459acfe6822
m.utkin <m.utkin@ac.gov.ru>
2024-02-09 14:16:41 +0300

    fix(pipeline): Revert from status:failed,skipped.

--- app/Helper/StatusHelper.php
+++ app/Helper/StatusHelper.php
@@ -13,7 +13,7 @@ class StatusHelper
     public static function isChange(string $current, string $next): bool
     {
         $statusStack = ['created', 'pending', 'running', 'failed', 'skipped', 'success'];
-        $resetStatus = ['success', 'failed', 'skipped'];
+        $resetStatus = ['failed', 'skipped'];
 
         $currentKey = array_search($current, $statusStack, true);
         $nextKey = array_search($next, $statusStack, true);
