85d7aea2d37f3fd01e79c050f08fc22e2478d63c
m.utkin <m.utkin@ac.gov.ru>
2024-02-12 11:35:03 +0300

    feat(pipe): Total time

--- resources/views/pipeline/default.blade.php
+++ resources/views/pipeline/default.blade.php
@@ -8,14 +8,14 @@
 {{$item['icon']}} CI: <a href="{{$item['url']}}">{{$item['name']}}</a> {{$item['status']}}
 @break
 @case('success')
-{{$item['icon']}} CI: <a href="{{$item['url']}}">{{$item['name']}}</a>  succeeded after {{round($item['duration'], 2)}} sec (in queue {{round($item['queued_duration'], 2)}} sec)
+{{$item['icon']}} CI: <a href="{{$item['url']}}">{{$item['name']}}</a>  succeeded after {{\Carbon\CarbonInterval::seconds($item['duration'])->cascade()->forHumans(short:true)}}
 @break
 @endswitch
 @endforeach
 
 @foreach($message as $id => $item)
 @if (!empty($item['total_duration']) > 0 && count($message) > 1)
-Total time: {{\Carbon\CarbonInterval::seconds($item['total_duration'])->cascade()->forHumans(short:true)}}
+Pipeline duration: {{\Carbon\CarbonInterval::seconds($item['total_duration'])->cascade()->forHumans(short:true)}}
 @break
 @endif
 @endforeach
